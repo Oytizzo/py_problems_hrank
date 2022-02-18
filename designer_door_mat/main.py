2dc6f30aa99d2d2d9132f4314e5d0dc026d7f8a9
oytizzo <oytizzo@gmail.com>
2022-02-19 00:24:45 +0600

    1 prob slvd

--- designer_door_mat/main.py
+++ designer_door_mat/main.py
@@ -1,7 +1,14 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 if __name__ == "__main__":
     a, b = input().split(" ")
-    for i in range(int(a)):
-        for j in range(int(b)):
-            print("-", end="")
-        print("\n")
+    a = int(a)
+    b = int(b)
+    c = '.|.'
+    message = 'WELCOME'
+    for i in range(a):
+        if i < int(a/2):
+            print((c * i).rjust(b//2-1, '-') + c + (c * i).ljust(b//2-1, '-'))
+        elif i == int(a/2):
+            print(message.center(b, '-'))
+        elif i > int(a/2):
+            print((c * (a-1-i)).rjust(b//2-1, '-') + c + (c * (a-1-i)).ljust(b//2-1, '-'))
